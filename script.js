e84d3610a459de888dbf35f866cfa61d5d39185e
Micael <micael9.moras@gmail.com>
2025-04-15 12:11:11 -0300

    coloquei minutos e segundos no codigo de java

--- script.js
+++ script.js
@@ -3,17 +3,22 @@
 let msg = window.document.getElementById('msg')
 let data = new Date()
 let hora = data.getHours()
+let minutos = data.getMinutes()
+let segundos = data.getSeconds()
+
+if (minutos < 10) minutos = "0" + minutos
+if (segundos < 10) segundos = "0" + segundos
 
 if (hora >= 0 && hora < 12){
-    msg.innerHTML = `Bom dia!! são ${hora} horas, Esperamos que sua manhã esteja sendo produtiva. Aproveite para conhecer nossos serviços!!`
+    msg.innerHTML = `Bom dia!! São ${hora}:${minutos}:${segundos}, Esperamos que sua manhã esteja sendo produtiva. Aproveite para conhecer nossos serviços!!`
 } 
 
 else if(hora >= 12 && hora < 18){
-    msg.innerHTML = `Boa tarde!! são ${hora} horas, que tal dar uma olhada nas novidades da nossa empresa agora à tarde? Estamos aqui pra te ajudar!`
+    msg.innerHTML = `Boa tarde!! São ${hora}:${minutos}:${segundos}, que tal dar uma olhada nas novidades da nossa empresa agora à tarde? Estamos aqui pra te ajudar!`
 }
 
 else{
-    msg.innerHTML = `Boa noite!! são ${hora} horas, mesmo à noite, seguimos prontos para atender você. Fique à vontade para explorar o nosso site!` 
+    msg.innerHTML = `Boa noite!! São ${hora}:${minutos}:${segundos}, mesmo à noite, seguimos prontos para atender você. Fique à vontade para explorar o nosso site!` 
 }
 
 // CÓDIGO PARA O ALERT DO FORMULARIO
