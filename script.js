a7964daef79978bced5e2b6c4ce3fc48e6cbb85d
Micael <micael9.moras@gmail.com>
2025-04-23 18:01:52 -0300

    tirei os segundos e coloquei mais 3 produtos pra ficar certinho

--- script.js
+++ script.js
@@ -4,21 +4,19 @@ let msg = window.document.getElementById('msg')
 let data = new Date()
 let hora = data.getHours()
 let minutos = data.getMinutes()
-let segundos = data.getSeconds()
 
 if (minutos < 10) minutos = "0" + minutos
-if (segundos < 10) segundos = "0" + segundos
 
 if (hora >= 0 && hora < 12){
-    msg.innerHTML = `Bom dia!! São ${hora}:${minutos}:${segundos}, Nas compras acima de R$100, o frete é grátis até o dia 30/04. Aproveite para conhecer nossos serviços!`
+    msg.innerHTML = `Bom dia!! São ${hora}:${minutos}. Em compras acima de R$100, o frete é por nossa conta até 30/04. Que tal aproveitar e conhecer tudo o que a gente oferece?`
 } 
 
 else if(hora >= 12 && hora < 18){
-    msg.innerHTML = `Boa tarde!! São ${hora}:${minutos}:${segundos}, Nas compras acima de R$100, o frete é grátis até o dia 30/04. Aproveite para conhecer nossos serviços!! `
+    msg.innerHTML = `Boa tarde!! São ${hora}:${minutos}. Em compras acima de R$100, o frete é por nossa conta até 30/04. Que tal aproveitar e conhecer tudo o que a gente oferece? `
 }
 
 else{
-    msg.innerHTML = `Boa noite!! São ${hora}:${minutos}:${segundos}, Nas compras acima de R$100, o frete é grátis até o dia 30/04. Aproveite para conhecer nossos serviços!!` 
+    msg.innerHTML = `Boa noite!! São ${hora}:${minutos}. Em compras acima de R$100, o frete é por nossa conta até 30/04. Que tal aproveitar e conhecer tudo o que a gente oferece?` 
 }
 
 // CÓDIGO PARA O ALERT DO FORMULARIO
